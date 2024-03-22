e2579fd9c22822c21e4844408f612a86c4231099
Juaneszq <118019819+Juanes704@users.noreply.github.com>
2024-03-22 11:53:45 -0500

    Update LivingBeingCreator.java

--- CreationOfLivingBeings/src/LivingBeingCreator.java
+++ CreationOfLivingBeings/src/LivingBeingCreator.java
@@ -22,6 +22,6 @@ public class LivingBeingCreator implements Creator {
 
     @Override
     public String toString() {
-        return "LivingBeingCreator{No funciona}";
+        return "LivingBeingCreator{Y se hizo la vida}";
     }
 }
